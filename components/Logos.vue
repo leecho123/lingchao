21b8272477f45ed501a0e5d8803826c2219dcb35
Avenue-opposites <2949195453@qq.com>
2024-04-15 09:23:40 +0800

    chore: 修正Logos组件

--- components/Logos.vue
+++ components/Logos.vue
@@ -1,7 +1,8 @@
 <template>
   <svg id="logos" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <path
-      stroke="primary 2" fill-none
+      class="stroke-primary"
+      stroke-width="2" fill="none"
       stroke-linecap="square"
       d="
         M3 6 C8 0 16 12 21 6
@@ -11,3 +12,34 @@
     />
   </svg>
 </template>
+
+<style scoped>
+  #logos path {
+  stroke-dashoffset: 1px;
+  stroke-dasharray: 0 30px;
+  animation: grow 5s ease forwards infinite;
+  transform-origin: center;
+}
+
+@keyframes grow {
+  0% {
+    stroke-dashoffset: 1px;
+    stroke-dasharray: 0 30px;
+    opacity: 0;
+  }
+  10% {
+    opacity: 1;
+  }
+  40% {
+    stroke-dasharray: 30px 0;
+  }
+  /* Moving back */
+  99% {
+    stroke-dasharray: 30px 0;
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
